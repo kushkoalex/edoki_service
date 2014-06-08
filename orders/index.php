e924d5bd8fa21853ea9274e2f4f503af099d8ad1
kushkoalexgit config --global user.email kushko.alex@gmail.com <kushko.alex@gmail.com>
2014-06-08 22:24:31 +0300

    -

--- orders/index.php
+++ orders/index.php
@@ -4,8 +4,26 @@ if (!isset($_SESSION['login_status'])) {
     header("Location:login.html");
 }
 require "init.php";
+
+if (isset($_POST['dishname'])
+    && isset($_POST['address'])
+    && isset($_POST['phone'])
+) {
+
+    $dishname = $_POST['dishname'];
+    $address = $_POST['address'];
+    $phone = $_POST['phone'];
+
+    $query = "insert into orders (dishname, phone, address,date) value('$dishname','$phone','$address',now())";
+    mysql_query($query, $connection) or die(mysql_error());
+}else
+{
+    echo("variables arent set");
+}
+
+
 $query = "select * from orders";
-$result = mysql_query($query, $connection);
+$result = mysql_query($query, $connection) or die(mysql_error());
 ?>
 
 <table>
@@ -21,6 +39,10 @@ $result = mysql_query($query, $connection);
     ?>
 </table>
 
-
-
+<form action="index.php" method="post">
+    <label for="dishname">dishname</label><input type="text" id="dishname" name="dishname"><br>
+    <label for="address">address</label><input type="text" id="address" name="address"><br>
+    <label for="phone">phone</label><input type="text" id="phone" name="phone"><br>
+    <input type="submit" value="Save">
+</form>
 

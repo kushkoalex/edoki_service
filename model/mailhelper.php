17280f810a7d29427c6f1fe37bc47492747c74d2
Alexander Kushko <kushko.alex@gmail.com>
2014-06-13 16:46:43 +0300

    changed mail body encoding to utf-8

--- model/mailhelper.php
+++ model/mailhelper.php
@@ -7,7 +7,8 @@ class MailHelper {
     public function sendMail($to,$from,$subj,$text)
     {
         $headers  = 'MIME-Version: 1.0' . "\r\n";
-        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+        //$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
         $headers .= 'From: '.$from. "\r\n";
         return mail($to, $subj, $text, $headers);
     }
